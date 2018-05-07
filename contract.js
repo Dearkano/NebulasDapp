6ccaf74585e5222a91812676e273ec7c777d1ecc
田子珺 <tzj912016486@vip.qq.com>
2018-05-08 02:13:28 +0800

    init

---
+++ contract.js
@@ -0,0 +1,89 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+var DepositeContent = /** @class */ (function () {
+    function DepositeContent(text) {
+        if (text) {
+            var o = JSON.parse(text);
+            this.balance = new BigNumber(o.balance);
+            this.expiryHeight = new BigNumber(o.expiryHeight);
+        }
+        else {
+            this.balance = new BigNumber(0);
+            this.expiryHeight = new BigNumber(0);
+        }
+    }
+    DepositeContent.prototype.toString = function () {
+        return JSON.stringify(this);
+    };
+    return DepositeContent;
+}());
+var BankVaultContract = /** @class */ (function () {
+    function BankVaultContract() {
+        LocalContractStorage.defineMapProperty(this, "bankVault", {
+            parse: function (text) {
+                return new DepositeContent(text);
+            },
+            stringify: function (o) {
+                return o.toString();
+            }
+        });
+    }
+    BankVaultContract.prototype.init = function () {
+        //TODO:
+    };
+    BankVaultContract.prototype.save = function (height) {
+        var from = Blockchain.transaction.from;
+        var value = Blockchain.transaction.value;
+        var bk_height = new BigNumber(Blockchain.block.height);
+        var orig_deposit = this.bankVault.get(from);
+        if (orig_deposit) {
+            value = value.plus(orig_deposit.balance);
+        }
+        var deposit = new DepositeContent();
+        deposit.balance = value;
+        deposit.expiryHeight = bk_height.plus(height);
+        this.bankVault.put(from, deposit);
+    };
+    BankVaultContract.prototype.takeout = function (value) {
+        var from = Blockchain.transaction.from;
+        var bk_height = new BigNumber(Blockchain.block.height);
+        var amount = new BigNumber(value);
+        var deposit = this.bankVault.get(from);
+        if (!deposit) {
+            throw new Error("No deposit before.");
+        }
+        if (bk_height.lt(deposit.expiryHeight)) {
+            throw new Error("Can not takeout before expiryHeight.");
+        }
+        if (amount.gt(deposit.balance)) {
+            throw new Error("Insufficient balance.");
+        }
+        var result = Blockchain.transfer(from, amount);
+        if (!result) {
+            throw new Error("transfer failed.");
+        }
+        Event.Trigger("BankVault", {
+            Transfer: {
+                from: Blockchain.transaction.to,
+                to: from,
+                value: amount.toString()
+            }
+        });
+        deposit.balance = deposit.balance.sub(amount);
+        this.bankVault.put(from, deposit);
+    };
+    BankVaultContract.prototype.balanceOf = function () {
+        var from = Blockchain.transaction.from;
+        return this.bankVault.get(from);
+    };
+    BankVaultContract.prototype.verifyAddress = function (address) {
+        // 1-valid, 0-invalid
+        var result = Blockchain.verifyAddress(address);
+        return {
+            valid: result == 0 ? false : true
+        };
+    };
+    return BankVaultContract;
+}());
+exports.BankVaultContract = BankVaultContract;
+//# sourceMappingURL=contract.js.map
