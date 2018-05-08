7e111ea01748cf05552f8629fdcc38a7262b94e8
田子珺 <tzj912016486@vip.qq.com>
2018-05-08 23:26:34 +0800

    update

--- contract.1.js
+++ contract.1.js
@@ -1,19 +1,11 @@
 
 "use strict";
 
-var ResumeContent = function (text) {
-    if (text) {
-        var o = JSON.parse(text);
-        this.resumeHash = text.resumeHash;
-        this.resume = text.resume;
-        this.owner = text.address;
-        this.nameHash = text.nameHash;
-    } else {
-        this.resume = "";
-        this.resumeHash = "";
-        this.owner = "";
-        this.nameHash="";
-    }
+var ResumeContent = function (from,nameHash,resume,resumeHash) {
+    this.owner = from;
+    this.resume = resume;
+    this.resumeHash = resumeHash;
+    this.nameHash=nameHash;
 };
 
 ResumeContent.prototype = {
@@ -38,26 +30,25 @@ ResumeContract.prototype = {
     init: function () {
     },
 
-    save: function (jsonstr) {
+    save: function (data) {
         var from = Blockchain.transaction.from;
-        var data1 = JSON.parse(jsonstr);
-        var data = data1[0];
-        var owner = this.resumedb.get(from);
-        if ((owner && owner == from) || owner=="") {
-            var resumeContent = new ResumeContent();
-            resumeContent.owner = from;
-            resumeContent.resume = data.resume;
-            resumeContent.resumeHash = data.resumeHash;
-            resumeContent.nameHash=data.nameHash;
-            this.resumedb.put(nameHash, JSON.stringify(resumeContent));
+        var fData = this.resumedb.get(data.nameHash);
+        if ((fData && fData.owner == from) || fData==null) {
+            var resumeContent = new ResumeContent(from,data.nameHash,data.resume,data.resumeHash);
+            this.resumedb.put(data.nameHash, JSON.stringify(resumeContent));
         }else{
             throw new Error("Cannot update this resume.");
         }
     },
-
+    testS:function(data){ return this.resumedb.get(data);},
+    testNameHashInput(data){
+        return data.nameHash;
+    },
+    testNameHashTest(data){
+        return data;
+    },
     query: function (nameHash) {
         var from = Blockchain.transaction.from;
-
         var resumeContent = JSON.parse(this.resumedb.get(nameHash));
 
         if(resumeContent==null){
--- contract.2.js
+++ contract.2.js
@@ -1,1 +1,65 @@
- \"use strict\";var ResumeContent = function (text) { if (text) { var o = JSON.parse(text); this.resumeHash = text.resumeHash; this.resume = text.resume; this.owner = text.address; this.nameHash = text.nameHash; } else { this.resume = \"\"; this.resumeHash = \"\"; this.owner = \"\"; this.nameHash= \"\"; }};ResumeContent.prototype = { toString: function () { return JSON.stringify(this); }};var ResumeContract = function () { LocalContractStorage.defineMapProperty(this, \"resumedb\", { parse: function (text) { return new ResumeContent(text); }, stringify: function (o) { return o.toString(); } });};ResumeContract.prototype = { init: function () {}, save: function (jsonstr) { var from = Blockchain.transaction.from; var data1 = JSON.parse(jsonstr); var data = data1[0]; var owner = this.resumedb.get(from); if ((owner && owner == from) || owner== \"\") { var resumeContent = new ResumeContent(); resumeContent.owner = from; resumeContent.resume = data.resume; resumeContent.resumeHash = data.resumeHash; resumeContent.nameHash=data.nameHash; this.resumedb.put(nameHash, JSON.stringify(resumeContent)); }else{ throw new Error( \"Cannot update this resume. \"); } }, query: function (nameHash) { var from = Blockchain.transaction.from; var resumeContent = JSON.parse(this.resumedb.get(nameHash)); if(resumeContent==null){ throw new Error( \"No resume before. \"); } if(from==resumeContent.owner){ return resumeContent; }else{ return resumeContent.resumeHash; } }};module.exports = ResumeContract;
+
+"use strict";
+
+var ResumeContent = function (from,nameHash,resume,resumeHash) {
+    this.owner = from;
+    this.resume = resume;
+    this.resumeHash = resumeHash;
+    this.nameHash=nameHash;
+};
+
+ResumeContent.prototype = {
+    toString: function () {
+        return JSON.stringify(this);
+    }
+};
+
+var ResumeContract = function () {
+    LocalContractStorage.defineMapProperty(this, "resumedb", {
+        parse: function (text) {
+            return new ResumeContent(text);
+        },
+        stringify: function (o) {
+            return o.toString();
+        }
+    });
+};
+
+
+ResumeContract.prototype = {
+    init: function () {
+    },
+
+    save: function (data) {
+        var from = Blockchain.transaction.from;
+        var fData = this.resumedb.get(data.nameHash);
+        if ((fData && fData.owner == from) || fData==null) {
+            var resumeContent = new ResumeContent(from,data.nameHash,data.resume,data.resumeHash);
+            this.resumedb.put(data.nameHash, JSON.stringify(resumeContent));
+        }else{
+            throw new Error("Cannot update this resume.");
+        }
+    },
+    testS:function(data){ return this.resumedb.get(data);},
+    testNameHashInput(data){
+        return data.nameHash;
+    },
+    testNameHashTest(data){
+        return data;
+    },
+    query: function (nameHash) {
+        var from = Blockchain.transaction.from;
+        var resumeContent = JSON.parse(this.resumedb.get(nameHash));
+
+        if(resumeContent==null){
+            throw new Error("No resume before.");
+        }
+
+        if(from==resumeContent.owner){
+            return resumeContent;
+        }else{
+            return resumeContent.resumeHash;
+        }
+    }
+};
+module.exports = ResumeContract;
